4fae90a31d203bc08d031df18d76075000592aa8
Iwona Lalak <ilalak@topsa.com.pl>
2019-12-19 16:03:13 +0100

    basic configuration, install packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faCat } from "@fortawesome/free-solid-svg-icons";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id="app">
+            <p>
+                <FontAwesomeIcon icon={faCat} />  Cat images app supported by <a href={"https://thecatapi.com"} target={"_blank"} style={{color: "dodgerblue"}}>CAT API</a>
+            </p>
+        </div>
+    );
 }
 
 export default App;
