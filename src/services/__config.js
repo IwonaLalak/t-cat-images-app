7e2ddacda4eaa7c36d5699a676949c6f515ae480
Iwona Lalak <ilalak@topsa.com.pl>
2020-02-21 16:02:13 +0100

    update

--- src/services/__config.js
+++ src/services/__config.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
+import {API_KEY} from "../hidden";
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
-axios.defaults.headers['x-api-key'] = '252c74fb-11f4-4e2a-89f5-00610724456f';
-
+axios.defaults.headers['x-api-key'] = API_KEY();
 
 export default {
-
     HOST_API: "https://api.thecatapi.com/v1/"
 }
